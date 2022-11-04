fd1223137a5290e27c91a27b55152dee8519370f
Mac <mac@Macs-MacBook-Pro.local>
2022-11-04 20:46:06 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        //Varargs
+        //method(10,15,1,10);
+        //Foreach
+
+       // int[] massive={1,2,3,4,5};
+
+        /*for (int i = 0; i < massive.length; i++) {
+            System.out.println(massive[i]);
+        }*/
+        /*for (int array:massive) {
+            System.out.println(array);
+        }*/
+        /*int sum=0;
+        for (int array:massive) {
+            sum=sum+array;
+        }
+           System.out.println(sum);*/
+        /*String[] massive={"Aimaral","Ainazik","Yntymak","Dosha"};
+        for (String array:massive) {
+            System.out.println("I love you "+array);
+        }*/
+
+        //String Methods
+       /* String word="Aimaral";
+        System.out.println(word.charAt(2)); //indextegi symboldu kaitarat
+        System.out.println(word.substring(1,3)); //1menen 3tyn arasyndagy symboldu kaitarat
+        System.out.println(word.contains("A")); //Soz kamtylganby jokpu korsotot
+        System.out.println(word.toUpperCase()); //Chon tamga menen jygarat*/
+ }
+    /*public static void method(int ... number){
+        int sum=0;
+        for (int i = 0; i < number.length; i++) {
+            sum=sum+number[i];
+        }
+        System.out.println(sum);
+    }*/
+
+
+
+}
